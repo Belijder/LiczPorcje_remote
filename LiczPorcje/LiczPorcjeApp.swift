0ff2218a52375b2db89b445e5ef1b57b8341bfc9
Jakub Zajda <jakubzajda@gmail.com>
2022-04-05 19:10:13 +0200

    Initial Commit

---
+++ LiczPorcje/LiczPorcjeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiczPorcjeApp.swift
+//  LiczPorcje
+//
+//  Created by Kamila Mroziewska on 05/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct LiczPorcjeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
